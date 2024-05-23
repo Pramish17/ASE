d302599eb37d9613b76621aebdafb6bed2fd0c36
PramishUWL <32129283@student.uwl.ac.uk>
2024-05-23 22:48:30 +0100

    port changes

--- airline-booking-backend/server.js
+++ airline-booking-backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 app.use(cors());
 app.use(express.json());
