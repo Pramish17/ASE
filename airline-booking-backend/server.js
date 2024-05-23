3aaefe503a85f7eac43639207e5bd496b4eb7fe5
PramishUWL <32129283@student.uwl.ac.uk>
2024-05-23 21:32:19 +0100

    code updated

--- airline-booking-backend/server.js
+++ airline-booking-backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = 4000;
